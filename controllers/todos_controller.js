7ec489ca21e6eaa3d96f93ec4b680bfae5703ea5
zhenhao goh <kenneth.gzhao@outlook.com>
2017-04-04 19:29:30 +0800

    finish tdd

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -2,33 +2,75 @@ const uuidGenerator = require('uuid/v4')
 const fs = require('fs')
 
 const todos = []
-//// the following line will instead load the todos from a json file when the app starts
+// // the following line will instead load the todos from a json file when the app starts
 // const todos = require('../data.json')
 
-//// The following function can be used to save the todos array to the json data file
+// // The following function can be used to save the todos array to the json data file
 // function save () {
 //   const json = JSON.stringify(todos)
 //   fs.writeFileSync('data.json', json, 'utf8')
 // }
 
 // CREATE - params should be an object with keys for name, description and completed
-function create (params) {
+function create (params, description = 'default', completed = false) {
+  var newTodo = params
+  if (newTodo.name.length < 5) { return false }
+  newTodo.description = description
+  newTodo.completed = completed
+  newTodo._id = uuidGenerator()
+  console.log(newTodo)
+  todos.push(newTodo)
 }
 
 // READ (list & show)
 function list () {
   // return list of all TODOs
+  return todos
 }
+
 function show (id) {
   // find the TODO with this id
+  // loop all todos
+  //    for each todo check for id
+  //
+  var todoindex = 0
+  todos.forEach(function (param, index) {
+    if (param._id === id) {
+      console.log(id)
+      todoindex = index
+    } else {
+      todoindex = null
+    }
+  })
+  if (todos[todoindex]) return todos[todoindex]
+  else {
+    return null
+  }
 }
 
 // UPDATE - params should be an object with KVPs for the fields to update
 function update (id, params) {
+  if (params.name.length < 5) { return false }
+  var theobject = show(id)
+  // console.log(params)
+  // var arrPos = todos.indexOf(theobject)
+  // todos[arrPos] = params
+  theobject.name = params.name
+  theobject.description = params.description
+  theobject.completed = params.completed
+  return true
 }
 
 // DESTROY (destroy & destroyAll)
 function destroy (id) {
+  var theobject = show(id)
+  var arrPos = todos.indexOf(theobject)
+  todos.splice(arrPos, 1)
+}
+
+function destroyAll () {
+  todos.splice(0)
+  return true
 }
 
 module.exports = {
@@ -36,5 +78,6 @@ module.exports = {
   list,
   show,
   update,
-  destroy
+  destroy,
+  destroyAll
 }
--- test/todo_tests.js
+++ test/todo_tests.js
@@ -1,5 +1,58 @@
 const assert = require('assert')
 const todos = require('../controllers/todos_controller.js')
 
+var testParams = {
+  name: 'abcde'
+}
+
+var testParams2 = {
+  name: 'abcd'
+}
+
+var testParams3 = {
+  name: 'hello world'
+}
+todos.create(testParams3)
+todos.create(testParams)
+todos.list()
+var lastTodos = todos.list()[todos.list().length-1]
+var lastId = lastTodos._id
 // // Use Assert to Test the functionality of all your CRUD methods e.g.
-// assert.strictEqual(todos.list().length, 0, 'List should return an array of all todos')
+// console.log(lastTodos);
+assert.strictEqual(lastTodos.completed,false,'completed not a boolean')
+assert.strictEqual(lastTodos.description,'default','completed not a boolean')
+assert.strictEqual(todos.create(testParams2),false,'name is less than 5 charecter')
+assert.strictEqual(todos.list().length, 2, 'List should return an array of all todos')
+//testign the todos show(id) method
+
+assert.strictEqual(todos.show(lastId),lastTodos,'show function not working')
+assert.strictEqual(todos.show(1234),null,'show function does not return null')
+
+// tseting update
+
+
+todos.update(lastId, {
+  name: 'bye world',
+  _id: lastId
+})
+assert.strictEqual(lastTodos.name,'bye world', 'Update function did not change name')
+
+assert.strictEqual(todos.update(lastId, {
+  name: '',
+  _id: lastId
+}),false, 'Update function did allowed name to be less than 5 charecters')
+
+assert.strictEqual(todos.update(lastId, {
+  name: 'code god',
+  _id: lastId
+}),true, 'Update function did return true when successful')
+
+
+//testing destroy function
+todos.destroy(lastId)
+
+assert.strictEqual(todos.show(lastId), null, 'destroy function did not delete the Todo with the given id')
+
+todos.destroyAll()
+assert.strictEqual(todos.list().length, 0, 'destroyAll function did not delete all objects in Todo')
+assert.strictEqual(todos.destroyAll(), true, 'destroyAll function did not delete all objects in Todo')
