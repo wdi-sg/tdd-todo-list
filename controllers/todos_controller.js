36779981edcd4a489c1cdf804e68361f2fb4bfe8
Darrell Teo <darrelltzj@gmail.com>
2017-04-04 17:59:26 +0800

    initial

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -13,18 +13,47 @@ const todos = []
 
 // CREATE - params should be an object with keys for name, description and completed
 function create (params) {
+  var newTodo = params
+  if (!newTodo.description || newTodo.description == '') {
+    newTodo.description = 'my description'
+  }
+  if (!newTodo.completed || newTodo.completed == '') {
+    newTodo.completed = false
+  }
+  newTodo._id = uuidGenerator()
+  if (newTodo.name && newTodo.name.length >= 5) {
+    todos.push(newTodo)
+  }
 }
 
 // READ (list & show)
 function list () {
   // return list of all TODOs
+  return todos
 }
 function show (id) {
   // find the TODO with this id
+  var output = null
+  todos.forEach(function (object) {
+    if (object._id == id) {
+      output = object
+    }
+  })
+  return output
 }
 
 // UPDATE - params should be an object with KVPs for the fields to update
 function update (id, params) {
+  var object = show(id)
+  if (params.name.length >= 5 && params.name !== '') {
+    for (key in params) {
+      params[key] = object[key]
+    }
+    return true
+  }
+  else {
+    return false
+  }
 }
 
 // DESTROY (destroy & destroyAll)
--- test/todo_tests.js
+++ test/todo_tests.js
@@ -2,4 +2,72 @@ const assert = require('assert')
 const todos = require('../controllers/todos_controller.js')
 
 // // Use Assert to Test the functionality of all your CRUD methods e.g.
-// assert.strictEqual(todos.list().length, 0, 'List should return an array of all todos')
+
+// Testing todos.list() function
+assert.strictEqual(todos.list().length, 0, 'List should return an array of all todos')
+
+// Testing the todo.create(params) method
+var testParams = {
+  name: 'testing',
+  description: 'my description abc',
+  completed: false
+}
+todos.create(testParams)
+assert.strictEqual(todos.list().length, 1, 'create should push an object into the todos list')
+
+// <TEST FOR DEFAULTS, NO NAME?>
+var testParams = {
+  name: 'testing2',
+  description: 'my description',
+  completed: false
+}
+todos.create(testParams)
+assert.strictEqual(todos.list().length, 2, 'create cannot accept a name of more than 5 characters')
+
+// Testing the name length in create(params)
+var testParams = {
+  name: 'less',
+  description: 'my description',
+  completed: false
+}
+todos.create(testParams)
+assert.strictEqual(todos.list().length, 2, 'create cannot accept a name of more than 5 characters')
+
+var testId = todos.list()[0]._id
+// Testing the todos.show(id) method
+assert.strictEqual(todos.show(testId), todos.list()[0], 'show should return todo of id')
+// Testing the todos.show(id) with no ID
+assert.strictEqual(todos.show('testId123'), null, 'show should return null')
+
+// Testing update ID
+var updateTestParams = {
+  name: 'testing3',
+  description: 'new description',
+  completed: true
+}
+assert.strictEqual(todos.update(testId, updateTestParams), true, 'update should return true')
+
+// Testing update ID (more than 5)
+var updateTest2Params = {
+  name: 'less',
+  description: 'new description',
+  completed: true
+}
+assert.strictEqual(todos.update(testId, updateTest2Params), false, 'update should return false')
+
+// Testing update ID (blank)
+var updateTest3Params = {
+  name: '',
+  description: 'new description',
+  completed: true
+}
+assert.strictEqual(todos.update(testId, updateTest3Params), false, 'update should return false')
+
+// Testing destroy
+console.log(todos.list(),testId)
+// todos.delete(id)
+assert.strictEqual(todos.delete(testId), true, 'delete should return true')
+
+//Check removed
+
+assert.strictEqual(todos.delete('testing123'), false, 'delete should return true')
