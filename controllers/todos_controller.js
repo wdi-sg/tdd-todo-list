ac32e8f744b271b2ced63e0ca413bf6e89726913
seth loh <haloandwings@gmail.com>
2017-04-04 21:27:14 +0800

    finished assignment and added success module

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -12,23 +12,84 @@ const todos = []
 // }
 
 // CREATE - params should be an object with keys for name, description and completed
-function create (params) {
+function create(params) {
+  var newTodo = params
+
+  newTodo._id = uuidGenerator()
+  console.log(params._id + " was created")
+  if (!newTodo.description) newTodo.description === "default"
+  if (!newTodo.completed) newTodo.completed === "false"
+  if (newTodo.name.length >= 5) {
+    todos.push(newTodo)
+  }
 }
-some changes
+
 // READ (list & show)
-function list () {
+function list() {
+  return todos
   // return list of all TODOs
 }
-function show (id) {
+
+function show(id) {
   // find the TODO with this id
+
+  var object = {}
+
+  todos.forEach(function(value) {
+    if (value._id === id) {
+      object = value
+    }
+  })
+
+  if (Object.keys(object).length === 0) return null
+  else return object
 }
 
 // UPDATE - params should be an object with KVPs for the fields to update
-function update (id, params) {
+function update(id, params) {
+
+  var updatedParams = params
+  updatedParams._id = id
+  var index = null
+
+  todos.forEach(function(value, ind) {
+    if (value._id === id) {
+      index = ind
+    }
+  })
+
+  if (!updatedParams.description) updatedParams.description === "default"
+  if (!updatedParams.completed) updatedParams.completed === "false"
+  if (updatedParams.name.length >= 5) {
+    todos[index] = updatedParams
+    return true
+  } else return false
 }
 
 // DESTROY (destroy & destroyAll)
-function destroy (id) {
+function destroy(id) {
+
+  var index = null
+
+  todos.forEach(function(value,ind) {
+    if (value._id === id) {
+      index = ind
+    }
+  })
+
+  if (index) {
+    todos.splice(index,1)
+    return true
+  }
+  else return false
+}
+
+function destroyAll() {
+  console.log('Deleting all todos!')
+  todos.forEach(function(val){
+      todos.pop()
+  })
+  return true
 }
 
 module.exports = {
@@ -36,5 +97,6 @@ module.exports = {
   list,
   show,
   update,
-  destroy
+  destroy,
+  destroyAll
 }
