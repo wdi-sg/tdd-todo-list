ab28646cef1c7d19b2725c4e98ab47b89c9e8f49
Shirong Foo <dee.sr.foo@gmail.com>
2017-04-04 19:55:01 +0800

    updated todos_controller.js and todo_tests.js files

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -1,11 +1,13 @@
 const uuidGenerator = require('uuid/v4')
 const fs = require('fs')
 
+// console.log(uuidGenerator);
+
 const todos = []
-//// the following line will instead load the todos from a json file when the app starts
+// // the following line will instead load the todos from a json file when the app starts
 // const todos = require('../data.json')
 
-//// The following function can be used to save the todos array to the json data file
+// // The following function can be used to save the todos array to the json data file
 // function save () {
 //   const json = JSON.stringify(todos)
 //   fs.writeFileSync('data.json', json, 'utf8')
@@ -13,23 +15,86 @@ const todos = []
 
 // CREATE - params should be an object with keys for name, description and completed
 function create (params) {
+  var newTodo = params
+  newTodo._id = uuidGenerator()
+  if (newTodo.description === null) {
+    newTodo.description = 'default'
+  } else if (newTodo.completed === null) {
+    newTodo.completed = false
+  } else if (newTodo.completed === null && newTodo.description === null) {
+    newTodo.completed = false
+    newTodo.description = 'default'
+  }
+
+  if (newTodo.name !== null && newTodo.name !== '' && newTodo.name.length > 5) {
+    todos.push(newTodo)
+     // console.log(newTodo);
+  }
 }
 
 // READ (list & show)
 function list () {
-  // return list of all TODOs
+  return todos
 }
 function show (id) {
   // find the TODO with this id
+  var array = list()
+  for (var i = 0; i < array.length; i++) {
+    if (array[i]._id == id) {
+      return array[i]
+    } else {
+      return null
+    }
+  }
 }
 
 // UPDATE - params should be an object with KVPs for the fields to update
 function update (id, params) {
+  var array = list()
+  var updated = 0
+  for (var i = 0; i < array.length; i++) {
+    if (array[i]._id = id) {
+      if (params.name > 5) {
+        array[i].name = params.name
+        updated = 1
+      }
+      array[i].description = params.description
+      array[i].completed = params.completed
+
+    } else {
+      updated = 0
+    }
+  }
+  if (updated === 1) {
+    return true
+  } else {
+    return false
+  }
 }
 
 // DESTROY (destroy & destroyAll)
 function destroy (id) {
+  var array = list()
+  for (var i = 0; i < array.length; i++) {
+    if (array[i]._id = id) {
+      array.splice(i, 1)
+      return true
+    } else {
+      return false
+    }
+  }
 }
+
+function destroyAll(){
+  todos = []
+  if(todos.length == 0){
+    return true
+  }
+  else {
+    return false
+  }
+}
+
 
 module.exports = {
   create,
--- test/todo_tests.js
+++ test/todo_tests.js
@@ -1,5 +1,75 @@
 const assert = require('assert')
-const todos = require('../controllers/todos_controller.js')
+const todosController = require('../controllers/todos_controller.js')
 
-// // Use Assert to Test the functionality of all your CRUD methods e.g.
-// assert.strictEqual(todos.list().length, 0, 'List should return an array of all todos')
+//testing the todo.create(params) methods
+var testParams1 = {
+  name: 'buy fish',
+  description: 'my description',
+  completed: false
+}
+
+var testParams2 = {
+  name: '',
+  description: 'my description',
+  completed: false
+}
+
+var testParams3 = {
+  name: 'Daniel'
+}
+
+var testParams4 = {
+  name: 'Da'
+}
+
+// # Testing create(params)  # //
+todosController.create(testParams1)
+assert.strictEqual(todosController.list().length, 1, 'one param should have been created')
+console.log('testParams1 passed!!!!!!');
+
+todosController.create(testParams2)
+assert.strictEqual(todosController.list().length, 1, 'params should not have been created')
+console.log('testParams2 passed!!!!!!');
+
+todosController.create(testParams3)
+assert.strictEqual(todosController.list().length, 2, 'one more params should have been created')
+console.log('testParams3 passed!!!!!!');
+
+todosController.create(testParams4)
+assert.strictEqual(todosController.list().length, 2, 'params should not have been created')
+console.log('testParams4 passed!!!!!!');
+
+
+assert.strictEqual(todosController.list().length, 2, 'params should not have been created')
+
+
+
+// # Testing show(id)  # //
+var showVar = todosController.show(todosController.list()[0]._id)
+assert.strictEqual(todosController.show(todosController.list()[0]._id), showVar, 'this should return buy fish' )
+console.log('passed show 1');
+
+var showVar = todosController.show(todosController.list()[1]._id)
+assert.strictEqual(todosController.show(todosController.list()[1]._id), showVar, 'this should return null' )
+console.log('passed show 2');
+
+// # Update(id, updatedParam) #//
+var updatedParam = {
+  name: 'buying',
+  description: 'large brown',
+  completed: false
+}
+
+todosController.create(updatedParam)
+var updateResult = todosController.update(todosController.show(todosController.list()[1]._id), todosController.list()[3])
+assert.strictEqual(todosController.update(todosController.show(todosController.list()[1]._id), todosController.list()[2]), updateResult, 'it should have updated')
+console.log('passed update');
+
+
+//# Destroy(id) #//
+var destroyVar = todosController.destroy(todosController.show(todosController.list()[1]._id))
+assert.strictEqual(todosController.destroy(todosController.show(todosController.list()[1]._id)), destroyVar, 'it should have been deleted')
+console.log('passed destroy');
+
+var destroyAllVar = todosController.destroyAll
+assert.strictEqual(todosController.destroyAll, destroyAllVar, 'all should have been deleted')
