c50934029ddc36100094ffe35f0b4e2dacf2d502
hexhex23 <mariawilson23@gmail.com>
2017-04-05 09:06:42 +0800

    trying

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -1,6 +1,8 @@
 const uuidGenerator = require('uuid/v4')
 const fs = require('fs')
 
+console.log('this is id generator ' + uuidGenerator())
+
 const todos = []
 //// the following line will instead load the todos from a json file when the app starts
 // const todos = require('../data.json')
@@ -13,14 +15,37 @@ const todos = []
 
 // CREATE - params should be an object with keys for name, description and completed
 function create (params) {
+  var newTodo = params
+  newTodo._id = uuidGenerator()
+  console.log(newTodo)
+  todos.push(newTodo)
+}
+
+//if name is undefined
+
+function checkName () {
+  if (testParamsName.name !== undefined) {
+    return true
+  } else if (testParamsName.name === undefined) {
+    return false
+  } else if (testParamsName.length <= 4) {
+    return false
+  }
 }
 
 // READ (list & show)
 function list () {
-  // return list of all TODOs
+  return todos
 }
+
+
 function show (id) {
-  // find the TODO with this id
+  //Should return the Todo Object with the specified `id`
+  //Should return null if no TODO with that `id` exists
+  if (testParams._id !== null) {
+  return 'insert code for specified id'
+} else if (testParams._id === null)
+  return null
 }
 
 // UPDATE - params should be an object with KVPs for the fields to update
@@ -33,6 +58,7 @@ function destroy (id) {
 
 module.exports = {
   create,
+  checkName,
   list,
   show,
   update,
