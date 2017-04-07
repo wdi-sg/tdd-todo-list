cd7ab32e851e12dec21e568a87e7c9cae4586197
Tom Lee <d.dorkblue@gmail.com>
2017-04-07 09:17:13 +0800

    updated todos_controller with mongoose

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -1,21 +1,34 @@
 const uuidGenerator = require('uuid/v4')
 const fs = require('fs')
-console.log(uuidGenerator())
-const todos = []
-// // the following line will instead load the todos from a json file when the app starts
+
+var Todo = require('../models/todo')
+var mongoose = require('mongoose')
+var dbURI = 'mongodb://localhost/exercise'
+mongoose.Promise = global.Promise
+
+mongoose.connect(dbURI)
+
+console.log('connected to db')
+
+//
+// // const todos = []
+// // // the following line will instead load the todos from a json file when the app starts
 // const todos = require('../data.json')
-
-// // The following function can be used to save the todos array to the json data file
-// function save () {
-//   const json = JSON.stringify(todos)
-//   fs.writeFileSync('data.json', json, 'utf8')
-// }
-
-// CREATE - params should be an object with keys for name, description and completed
+//
+// // // The following function can be used to save the todos array to the json data file
+function save (input) {
+  input.save(function (err, data) {
+    if (err) console.error(err)
+    console.log('new To-do ' + data.name + ' is created')
+    mongoose.disconnect()
+  })
+}
+//
+// // CREATE - params should be an object with keys for name, description and completed
 function create (params) {
-  var newTodo = {}
+  var newTodo = new Todo()
   if (params.name === undefined || params.name.length < 5) {
-    // console.log('NAME NOT GIVEN! Cannot create new Todo!')
+    console.log('Name not give! || name shorter than 5 characters.\nCannot create new Todo!')
     return false
   } else {
     newTodo.name = params.name
@@ -32,85 +45,72 @@ function create (params) {
   } else {
     newTodo.completed = params.completed
   }
+  save(newTodo)
 
-  newTodo._id = uuidGenerator()
-
-  todos.push(newTodo)
-  return newTodo
+  // newTodo._id = uuidGenerator()
+  // return newTodo
 }
 
-// function create (params) {
-//   var newTodo = {
-//     name: function (inputName) {
-//       if (inputName === undefined) {
-//         console.log('To-do name not given!')
-//         return false
-//       } else {
-//         return inputName
-//       }
-//     },
-//     description: params.description,
-//     completed: params.completed
-//
-//   }
-//   return newTodo.name(params.name)
-//   console.log(newTodo)
-// }
-
-// READ (list & show)
+// LIST - list todos
 function list () {
   // return list of all TODOs
-  return todos
-}
-function show (id) {
-  // find the TODO with this id
-  var toShow = null
-  todos.forEach(function (item, index, array) {
-    if (item._id === id) {
-      toShow = item
-    }
+  return Todo.find().exec(function (err, data) {
+    if (err) console.error(err)
+    console.log(data)
+    mongoose.disconnect()
+    return data
   })
-
-  return toShow
 }
 
+// SHOW - show todos based on id
+function show (id) {
+  console.log('Showing Todo based on id')
+  // find the TODO with this id
+  return Todo.find().where('_id').equals(id).exec(function (err, data) {
+    if (err) console.error(err)
+    console.log(data)
+    mongoose.disconnect()
+    return data
+  })
+}
+
+// MyModel.update({ age: { $gt: 18 } }, { oldEnough: true }, fn);
 // UPDATE - params should be an object with KVPs for the fields to update
 function update (id, params) {
-  var toUpdate = show(id)
+  var toUpdate = {}
 
   if ((params.name !== undefined && params.name === '') || (params.name !== undefined && params.name.length < 5)) {
+    console.log('Update failed')
     return false
   } else {
-    toUpdate.name = params.name
+    params.name === undefined ? '' : toUpdate.name = params.name
   }
-
-  params.name === undefined ? '' : toUpdate.name = params.name
   params.description === undefined ? '' : toUpdate.description = params.description
   params.completed === undefined ? '' : toUpdate.completed = params.completed
-  return true
-}
 
-// DESTROY (destroy & destroyAll)
-function destroy (id) {
-  var toDestroy = show(id)
-
-  if (toDestroy) {
-    var index = todos.indexOf(toDestroy)
-
-    todos.splice(index, 1)
-    return true
-    console.log(todos)
-  } else {
-    return false
-  }
-}
-
-function destroyall () {
-  todos.forEach(function (item, index, array) {
-    array.pop()
+  Todo.update({ _id: id}, toUpdate, function (err, data) {
+    if (err) console.error(err)
+    console.log('Update successful')
+    console.log(data)
   })
-
   return true
+}
+// DESTROY todos based on id
+function destroy (id) {
+  Todo.remove({_id: id}, function (err) {
+    if (err) console.error(err)
+    console.log('Destroyed!')
+    return true
+  })
+}
+
+// DESTROYALL todos
+function destroyall () {
+  Todo.remove({}, function (err) {
+    if (err) console.error(err)
+    console.log('All destroyed!')
+    return true
+  })
 }
 
 module.exports = {
