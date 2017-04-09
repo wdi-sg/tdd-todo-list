30d78f42b949d97209d06c23d8bfa6a94bd4815b
seth loh <haloandwings@gmail.com>
2017-04-09 14:44:14 +0800

    bad one

--- app.js
+++ app.js
@@ -27,24 +27,45 @@ app.use(ejsLayouts)
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: false}))
 
-
-
+/* Routes begin here
+++++++++++++++++++++++++++++++ */
 app.get('/',function(req,res){
   Todos.find({}, function(err,todos){
     if (err) throw console.error(err);
-    console.log(todos)
+    // console.log(todos)
     res.render('homepage', {allTodos: todos})
   })
 })
 
 // getting a todo by id
 app.get('/:id',function(req,res){
-  console.log(req.params.id)
-  Todos.findById(req.params.id,function(err,todos){
+  console.log('connected to ' +req.params.id)
+  Todos.find({ _id: req.params.id},function(err,todos){
     if (err) throw console.error(err);
-    console.log(todos)
-    res.render('homepage', {allTodos: todos})
+    // console.log(todos)
+    res.render('todo', {allTodos: todos})
   })
+})
+
+//updating
+app.post('/update',function(req,res){
+  //read the output
+  console.log(req.body)
+  Todos.update({_id: req.body.id}, {$set:{name: req.body.name, description: req.body.description, completed: req.body.completed}},function(err){
+    if (err) throw console.error(err);
+    console.log('updated '+req.body.id+' todo')
+  })
+  res.redirect('back')
+})
+
+app.post('/delete',function(req,res){
+  //read the output
+  console.log(req.body)
+  Todos.remove({_id: req.body.id}, function(err){
+    if (err) throw console.error(err);
+    console.log('Deleted '+req.body.id)
+  })
+  res.redirect('/')
 })
 
 // adding a new todo
@@ -61,7 +82,7 @@ app.post('/',function(req,res){
     if (err) throw console.error(err);
     console.log('saved new todo')
   })
-  res.redirect(req.get('referer')) // super useful for going back to the page
+  res.redirect('/') // super useful for going back to the page
 })
 
 app.listen(port, function(){
