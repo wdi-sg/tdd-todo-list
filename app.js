332c7154482c4c41ef8088b249f3fae7862ae1fa
seth loh <haloandwings@gmail.com>
2017-04-09 16:41:55 +0800

    working except for delete all button

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ var port = 5000
 var mongoose = require('mongoose')
 var dbURI = 'mongodb://localhost/tdd'
 
-mongoose.connect(dbURI,function(err){
+mongoose.connect(dbURI, function(err) {
   if (err) throw console.error(err);
-  else console.log('connected to '+dbURI)
+  else console.log('connected to ' + dbURI)
 })
 
 // Initializing the models
@@ -17,7 +17,7 @@ mongoose.Promise = global.Promise
 var Todos = require('./models/todos')
 
 // setting up template engine for express
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 
 //setting my layout structure
 var ejsLayouts = require('express-ejs-layouts')
@@ -25,60 +25,94 @@ app.use(ejsLayouts)
 
 // handle the post request
 var bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 
 /* Routes begin here
 ++++++++++++++++++++++++++++++ */
-app.get('/',function(req,res){
-  Todos.find({}, function(err,todos){
+app.get('/', function(req, res) {
+  Todos.find({}, function(err, todos) {
     if (err) throw console.error(err);
     // console.log(todos)
-    res.render('homepage', {allTodos: todos})
+    res.render('homepage', {
+      allTodos: todos
+    })
   })
 })
 
 // getting a todo by id
-app.get('/:id',function(req,res){
-  console.log('redirecting to ' +req.params.id)
-  Todos.find({ _id: req.params.id},function(err,todos){
+app.get('/:id', function(req, res) {
+  console.log('redirecting to ' + req.params.id)
+  Todos.find({
+    _id: req.params.id
+  }, function(err, todos) {
     if (err) throw console.error(err);
     // console.log(todos)
-    res.render('todo', {allTodos: todos})
+    res.render('todo', {
+      allTodos: todos
+    })
   })
 })
 
-app.get('/id',function(req,res){
-  console.log('redirecting to ' +req.body.id)
-  Todos.find({ _id: req.body.id},function(err,todos){
+// posting a todo by id
+app.post('/:id', function(req, res) {
+  console.log('redirecting to ' + req.params.id)
+  Todos.find({
+    _id: req.params.id
+  }, function(err, todos) {
     if (err) throw console.error(err);
     // console.log(todos)
-    res.render('todo', {allTodos: todos})
+    res.render('todo', {
+      allTodos: todos
+    })
   })
 })
 
-//updating
-app.post('/update',function(req,res){
+// adding a post route for delete
+app.post('/update', function(req, res) {
   //read the output
   console.log(req.body)
-  Todos.update({_id: req.body.id}, {$set:{name: req.body.name, description: req.body.description, completed: req.body.completed}},function(err){
+  Todos.update({
+    _id: req.body.id
+  }, {
+    $set: {
+      name: req.body.name,
+      description: req.body.description,
+      completed: req.body.completed
+    }
+  }, function(err) {
     if (err) throw console.error(err);
-    console.log('updated '+req.body.id+' todo')
+    console.log('updated ' + req.body.id + ' todo')
   })
   res.redirect('back')
 })
 
-app.post('/delete',function(req,res){
+// adding a post route for delete
+app.post('/delete', function(req, res) {
   //read the output
   console.log(req.body)
-  Todos.remove({_id: req.body.id}, function(err){
+  Todos.remove({
+    _id: req.body.id
+  }, function(err) {
     if (err) throw console.error(err);
-    console.log('Deleted '+req.body.id)
+    console.log('Deleted ' + req.body.id)
   })
   res.redirect('/')
 })
 
-// adding a new todo
-app.post('/create',function(req,res){
+// adding a post route for delete
+// app.post('/deleteAll', function(req, res) {
+//   //read the output
+//   Todos.remove({}, function(err) {
+//     if (err) throw console.error(err);
+//     console.log('Deleted All')
+//   })
+//   res.redirect('/')
+// })
+
+// adding a post route for create new todo
+app.post('/create', function(req, res) {
   //read the output
   console.log(req.body)
   var todo = new Todos({
@@ -86,13 +120,14 @@ app.post('/create',function(req,res){
     description: req.body.description,
     completed: req.body.completed
   })
-  todo.save(function(err){
+  todo.save(function(err) {
     if (err) throw console.error(err);
     console.log('saved new todo')
   })
   res.redirect('/') // super useful for going back to the page
 })
 
-app.listen(port, function(){
-  console.log('express is running on port '+ port)
+// defining the port that express is running on
+app.listen(port, function() {
+  console.log('express is running on port ' + port)
 })
