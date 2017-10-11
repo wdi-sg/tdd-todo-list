fd96bd25fbd5e0a8642297a5d42e9fd8a001e936
siya-ng <ngsiya90@gmail.com>
2017-10-11 23:06:58 +0800

    first commit

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -13,22 +13,69 @@ const todos = []
 
 // CREATE - params should be an object with keys for name, description and completed
 function create (params) {
+  if (params.name && params.name.length >= 5){
+    if (!params.description){ params.description='N/A'}
+    if (!params.completed){ params.completed=false}
+    params._id = uuidGenerator()
+    todos.push(params)
+    return true
+  }
+  else return false
+
+
 }
 
 // READ (list & show)
 function list () {
-  // return list of all TODOs
+  return todos
 }
 function show (id) {
-  // find the TODO with this id
+  var theItem = null
+  todos.forEach((item)=>{
+    if (item._id === id){
+      theItem = item
+    }
+  })
+  return theItem
 }
 
 // UPDATE - params should be an object with KVPs for the fields to update
 function update (id, params) {
+  var toUpdate = show(id)
+  if (params.name && params.name.length >= 5){
+    toUpdate.name = params.name
+    if (params.description) toUpdate.description = params.description
+    if (params.completed) toUpdate.completed = params.completed
+    toUpdate._id = id
+    return true
+  }
+  else return false
+    //return toUpdate
 }
 
 // DESTROY (destroy & destroyAll)
 function destroy (id) {
+  var currentLength = todos.length
+  var toDeleteItem = show(id)
+  var indexNo = todos.indexOf(toDeleteItem)
+  todos.splice(indexNo,1)
+  if (todos.length<currentLength){
+    return true
+  } else {
+    return false
+  }
+}
+
+function destroyAll(){
+  todos.length = 0
+  return true
+  // var currentLength = todos.length
+  // for (var i = 1; i < currentLength; i++){
+  //   todos.pop()
+  // }
+  // if (todos.length === 0){
+  //   return true
+  // } else return false
 }
 
 module.exports = {
@@ -36,5 +83,6 @@ module.exports = {
   list,
   show,
   update,
-  destroy
+  destroy,
+  destroyAll
 }
