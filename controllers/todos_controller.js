952b8543e622b40efb6a18631432c1549247de78
sm <a0086845@u.nus.edu>
2017-10-11 18:35:36 +0800

    up to destroy

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -1,5 +1,5 @@
-const uuidGenerator = require('uuid/v4')
-const fs = require('fs')
+const uuidGenerator = require("uuid/v4")
+const fs = require("fs")
 
 const todos = []
 // // the following line will instead load the todos from a json file when the app starts
@@ -12,23 +12,47 @@ const todos = []
 // }
 
 // CREATE - params should be an object with keys for name, description and completed
-function create (params) {
+function create(params) {
+  if (params.name === undefined || params.name.length < 5) return false
+  if (params.description === undefined) params.description = "NA"
+  if (params.completed === undefined) params.completed = false
+  params._id = uuidGenerator()
+  todos.push(params)
 }
 
 // READ (list & show)
-function list () {
+function list() {
+  return todos
   // return list of all TODOs
 }
-function show (id) {
+function show(id) {
   // find the TODO with this id
+  for (key in todos) {
+    if (todos[key]._id === id) return todos[key]
+  }
+  return null
 }
 
 // UPDATE - params should be an object with KVPs for the fields to update
-function update (id, params) {
+function update(id, params) {
+  if (params.name.length < 5) return false
+  for (key in todos) {
+    if (todos[key]._id === id) {
+      todos[key].name = params.name
+      if (params.description !== undefined)
+        todos[key].description = params.description
+      if (params.completed !== undefined)
+        todos[key].completed = params.completed
+    }
+  }
+  return true
 }
 
 // DESTROY (destroy & destroyAll)
-function destroy (id) {
+function destroy(id) {
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i]._id === id) todos.splice(i, 1)
+  }
 }
 
 module.exports = {
