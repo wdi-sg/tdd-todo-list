7a9a6ea07b38a3860aa5e50f6f7d617f752521a3
Peh Hui Shi <pehhuishi06@hotmail.com>
2017-10-12 13:55:57 +0800

    HS commit

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -13,22 +13,62 @@ const todos = []
 
 // CREATE - params should be an object with keys for name, description and completed
 function create (params) {
+  console.log("params given",params);
+  if(!params.name) return false
+  if(params.name.length <= 4) return false
+  if(!params.description) params.description = 'N/A'
+  if(!params.completed) params.completed = false
+  //set params key = _id = 12
+  params._id = uuidGenerator()
+
+  //console.log("params now",params)
+  todos.push(params)
 }
 
 // READ (list & show)
 function list () {
   // return list of all TODOs
+  return todos;
+
 }
 function show (id) {
   // find the TODO with this id
+  var obj = null
+  todos.forEach((item) => {
+    if(item._id === id){
+      obj = item
+    }
+  })
+  return obj
 }
 
 // UPDATE - params should be an object with KVPs for the fields to update
 function update (id, params) {
+  var item = show(id)
+  if(params.name.length >= 5){
+    item.name = params.name
+    item.description = params.description
+    item.completed = params.completed
+      return true
+  }else{
+    return false
+  }
 }
 
 // DESTROY (destroy & destroyAll)
 function destroy (id) {
+  var item = show(id)
+  if(item._id === id){
+    todos.splice(todos.indexOf(item),1)
+    return true
+  }else{
+    return false
+  }
+}
+
+function destroyAll(){
+  todos.splice(0,todos.length)
+  return true
 }
 
 module.exports = {
@@ -36,5 +76,6 @@ module.exports = {
   list,
   show,
   update,
-  destroy
+  destroy,
+  destroyAll
 }
