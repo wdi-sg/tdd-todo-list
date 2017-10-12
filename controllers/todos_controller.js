0719336ff0c4a4c4f591117e5b6485cdc41765d8
hello <31865878+mingyikoh@users.noreply.github.com>
2017-10-12 13:55:19 +0800

    TDD tests complete

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -13,22 +13,58 @@ const todos = []
 
 // CREATE - params should be an object with keys for name, description and completed
 function create (params) {
+  if (!params.name || params.name.length < 5) return false
+  if (!params.description) params.description = 'N/A'
+  if (!params.completed) params.completed = false
+  params._id = uuidGenerator()
+  todos.push(params)
 }
 
 // READ (list & show)
 function list () {
   // return list of all TODOs
+  return todos
 }
+
 function show (id) {
   // find the TODO with this id
+  var showItem = null
+  for (var i = 0; i < todos.length; i++) {
+    if (todos[i]._id === id) {
+      showItem = todos[i]
+    }
+  }
+  return showItem
 }
 
 // UPDATE - params should be an object with KVPs for the fields to update
 function update (id, params) {
+  if (!params.name || params.name.length < 5) return false
+  todos.forEach((item) => {
+    if (item._id === id) {
+      item.name = params.name
+      item.description = params.description
+      item.completed = params.completed
+      return true
+    } else {
+      return false
+    }
+  })
 }
 
 // DESTROY (destroy & destroyAll)
 function destroy (id) {
+  todos.forEach((item) => {
+    if (item._id === id) {
+      todos.splice(todos.indexOf(item), 1)
+      return true
+    }
+  })
+  return false
+}
+
+function destroyAll () {
+  todos.splice(0, todos.length)
 }
 
 module.exports = {
@@ -36,5 +72,6 @@ module.exports = {
   list,
   show,
   update,
-  destroy
+  destroy,
+  destroyAll
 }
--- test/todo_tests.js
+++ test/todo_tests.js
@@ -1,5 +1,102 @@
 const assert = require('assert')
 const todos = require('../controllers/todos_controller.js')
+const success = require('./helpers/success')
 
 // // Use Assert to Test the functionality of all your CRUD methods e.g.
-// assert.strictEqual(todos.list().length, 0, 'List should return an array of all todos')
+console.log('testing list method')
+assert.strictEqual(todos.list().length, 0, 'List should return an array of all todos')
+
+success()
+
+console.log('testing create method')
+
+var params = {
+  name: 'get a milk',
+  description: 'from cold storage',
+  completed: false
+}
+
+var params2 = {
+  name: 'get a milk2',
+  description: 'from cold storage2',
+  completed: false
+}
+
+var paramsJustName = {
+  name: 'buy a flower'
+}
+
+var paramsShortName = {
+  name: 'test'
+}
+
+var paramsNoName = {}
+
+var paramsUpdated = {
+  name: 'updatedName',
+  description: 'updatedDesc',
+  completed: 'false'
+}
+
+var todoListNow = todos.list()
+todos.create(params)
+assert.strictEqual(todoListNow.length, 1, 'Should add the todos arr')
+
+var createdItem = todoListNow[0]
+var keys = Object.keys(createdItem)
+
+keys.forEach(key => {
+  console.log(`check if the todo items created have property ${key}`)
+  assert.strictEqual(createdItem.hasOwnProperty(key), true, `key: ${key} is not found`)
+})
+success()
+
+console.log('check if a new todo items have different _id')
+todos.create(params2)
+var secondItem = todoListNow[1]
+assert.strictEqual(createdItem._id === secondItem._id, false, 'each _id should be unique')
+success()
+
+console.log('check if a new todo with only name property can be created, with default description and completed')
+todos.create(paramsJustName)
+var noNameItem = todoListNow[2]
+assert.strictEqual(noNameItem.description, 'N/A', 'default description value is not set')
+assert.strictEqual(noNameItem.completed, false, 'default completed value is not set')
+success()
+
+console.log('Invalid name property check')
+console.log('Testing blank name')
+assert.strictEqual(todos.create(paramsNoName), false, 'Name cannot be blank')
+success()
+
+console.log('Testing short name')
+assert.strictEqual(todos.create(paramsShortName), false, 'Name is too long')
+success()
+
+console.log('Testing list()')
+assert.strictEqual(todos.list().length, 3, 'list() should return todoListNow.length')
+success()
+
+console.log('Testing show(id)')
+assert.strictEqual(todos.show(todoListNow[1]._id), secondItem, 'show(id) should show obj with id')
+assert.strictEqual(todos.show('blahblah'), null)
+success()
+
+console.log('Testing update(id, params)')
+todos.update(todoListNow[0]._id, paramsUpdated)
+assert.strictEqual(todoListNow[0].name, 'updatedName', 'update(id, params) should update params of id')
+success()
+
+console.log('Testing destroy(id)')
+todos.destroy(todoListNow[0]._id)
+assert.strictEqual(todoListNow[0].name, 'get a milk2', 'destroy(id) did not remove')
+success()
+
+console.log('Testing destroy(id) without id match')
+assert.strictEqual(todos.destroy('blahblah'), false, 'destroy should return false if no match to id')
+success()
+
+console.log('Testing destroyAll()')
+todos.destroyAll()
+assert.strictEqual(todoListNow.length, 0)
+success()
