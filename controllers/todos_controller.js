222f0ab1caab3ce901d05f1b0c7383b619121c80
koozy <koozhengyu@gmail.com>
2017-10-12 09:36:05 +0800

    first commit

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -1,7 +1,7 @@
-const uuidGenerator = require('uuid/v4')
-const fs = require('fs')
+const uuidGenerator = require('uuid/v4');
+const fs = require('fs');
 
-const todos = []
+const todos = [];
 // // the following line will instead load the todos from a json file when the app starts
 // const todos = require('../data.json')
 
@@ -13,18 +13,43 @@ const todos = []
 
 // CREATE - params should be an object with keys for name, description and completed
 function create (params) {
+  if (!(params.hasOwnProperty('name') && params.name.length > 4)) return false;
+  // console.log('params given', params)
+  params._id = uuidGenerator();
+  // console.log('params now', params)
+  if (!params.hasOwnProperty('description')) params.description = 'default';
+  if (!params.hasOwnProperty('completed')) params.completed = false;
+  todos.push(params);
 }
 
 // READ (list & show)
 function list () {
   // return list of all TODOs
+  return todos;
 }
 function show (id) {
-  // find the TODO with this id
+  var obj = null;
+
+  todos.forEach( function (item) {
+    if (item._id === id) obj = item;
+  })
+
+  return obj;
 }
 
 // UPDATE - params should be an object with KVPs for the fields to update
 function update (id, params) {
+  if (params.name.length < 5) return false;
+  params._id = id;
+  todos.forEach( function (item, index, array) {
+    if (item._id === id) {
+      for (var key in array[index]) {
+        if(item[key] !== params[key] && params[key] !== undefined) array[index][key] = params[key];
+      }
+      // array[index] = params;
+    }
+  })
+  return true;
 }
 
 // DESTROY (destroy & destroyAll)
@@ -37,4 +62,4 @@ module.exports = {
   show,
   update,
   destroy
-}
+};
--- test/todo_tests.js
+++ test/todo_tests.js
@@ -1,5 +1,138 @@
-const assert = require('assert')
-const todos = require('../controllers/todos_controller.js')
-
+const assert = require('assert');
+const todos = require('../controllers/todos_controller.js');
+const success = require('./helpers/success');
 // // Use Assert to Test the functionality of all your CRUD methods e.g.
-// assert.strictEqual(todos.list().length, 0, 'List should return an array of all todos')
+
+// testing list
+console.log('testing list method')
+// test
+assert.strictEqual(todos.list().length, 0, 'List should return an array of all todos');
+// list test complete
+success();
+
+// testing create
+var params = {
+  name: 'get milk',
+  description: 'from cold storage',
+  completed: false
+};
+
+var params2 = {
+  name: 'get milk',
+  description: 'from cold storage',
+  completed: false
+};
+
+var params3 = {
+  name: 'testing'
+};
+
+var params4 = {};
+
+var params5 = { name: 'test' };
+
+// checking if todo has been added to array
+console.log('testing create method');
+// prep
+todos.create(params);
+var todoListNow = todos.list();
+// test
+assert.strictEqual(todoListNow.length, 1, 'Should add to the todos array');
+// checking if todo items have a _id
+console.log('checking if todo items created have a property _id');
+// prep
+var firstItem = todoListNow[0];
+// test
+assert.strictEqual(firstItem.hasOwnProperty('_id'), true, '_id is not found');
+
+// checking if todo items have unique _id
+console.log('checking if each todo item has a unique id');
+// prep
+todos.create(params2);
+var secondItem = todoListNow[1];
+// test
+assert.strictEqual(firstItem._id === secondItem._id, false, 'each todo item should have it\'s own unique _id');
+
+// checking if defaults are assigned for other fields
+console.log('checking if defaults are assigned for other fields');
+// prep
+todos.create(params3);
+var thirdItem = todoListNow[2];
+// tests
+assert.strictEqual(thirdItem.hasOwnProperty('description'), true, 'description field should be assigned a default value if not supplied');
+assert.strictEqual(thirdItem.hasOwnProperty('completed'), true, 'completed field should be assigned a default value if not supplied');
+
+// checking if item can be added to todos without a name
+console.log('checking if item can be added to todos without a name');
+// test
+assert.strictEqual(todos.create(params4), false, 'item should have a name');
+
+// checking if item can be added to todos if name is less than 5 characters long
+console.log('checking if item can be added to todos if name is less than 5 chars long');
+// test
+assert.strictEqual(todos.create(params5), false, 'item requires a name more than 4 chars long');
+
+// create test completed
+success();
+
+// testing show
+console.log('testing show method');
+// testing if show returns an object
+console.log('checking if show returns the correct object');
+// prep
+var id = params._id;
+var showParams = todos.show(id);
+// test
+assert.strictEqual(showParams, params, 'show should return the correct object');
+// testing if show returns null if incorrect id is supplied
+console.log('checking if show returns null if incorrect id is supplied');
+// test
+assert.strictEqual(todos.show('test'), null, 'show should return null if an incorrect id is supplied');
+// show test completed
+success();
+
+// testing update
+console.log('testing update method');
+// testing if able to update with new object
+console.log('checking if update changes the object');
+// prep
+var updatedParam = {
+  name: 'chocolate',
+  description: 'from marketplace',
+  completed: false
+};
+todos.update(id, updatedParam);
+var newParam = todoListNow[0];
+// test
+assert.strictEqual(newParam.name === updatedParam.name && newParam.description === updatedParam.description && newParam.completed === updatedParam.completed, true, 'update should change the original object to the supplied object');
+// testing if individual fields can be updated
+console.log('checking if update can update individual fields');
+// prep
+var updatedParam2 = { name: 'potato' };
+todos.update(id, updatedParam2);
+newParam = todoListNow[0];
+// test
+assert.strictEqual(newParam.name === updatedParam2.name && newParam.description === updatedParam.description && newParam.completed === updatedParam.completed && newParam._id === id, true, 'update should only update the updated fields');
+
+// testing if update disallows a name change if less than 5 chars long
+console.log('checking if update will allow a name change to less than five characters long');
+// prep
+var updatedParam3 = { name: 'test' };
+todos.update(id, updatedParam3);
+newParam = todoListNow[0];
+// test
+assert.strictEqual(newParam.name === updatedParam3.name, false, 'update should not allow a name change to less than  five characters long');
+
+// testing if update returns false upon unsuccessful call
+console.log('checking if update returns false when unsuccessful');
+// test
+assert.strictEqual(todos.update(id, updatedParam3), false, 'update should return false if unsuccessful');
+// testing if update returns true upon successful call
+console.log('checking if update returns true upon successful call');
+// prep
+var updatedParam4 = { name: 'tomato' };
+// test
+assert.strictEqual(todos.update(id, updatedParam4), true, 'update should return true if successful');
+
+// update test completed
+success();
