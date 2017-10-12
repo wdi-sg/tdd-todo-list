989061393c6a5b273975fd84e5422e50eca08636
heyzernut <hazeltoh@Hazels-MacBook-Pro.local>
2017-10-12 13:55:19 +0800

    submission

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -13,22 +13,61 @@ const todos = []
 
 // CREATE - params should be an object with keys for name, description and completed
 function create (params) {
+  if (!params.name) return false
+  if (params.name.length <= 4) return false
+
+  if (!params.description) params.description = 'N/A'
+  if (!params.completed) params.completed = false
+
+  params._id = uuidGenerator()
+  todos.push(params)
 }
 
 // READ (list & show)
 function list () {
-  // return list of all TODOs
+// return list of all TODOs
+  return todos
 }
+
 function show (id) {
-  // find the TODO with this id
+// find the TODO with this id
+  for (var i = 0; i < todos.length; i++) {
+    if (todos[i]._id === id) {
+      return todos[i]
+    }
+  }
+  return null
 }
 
 // UPDATE - params should be an object with KVPs for the fields to update
 function update (id, params) {
+  if (!params.name) return false
+  if (params.name.length <= 4) return false
+  for (var j = 0; j < todos.length; j++) {
+    if (todos[j]._id === id) {
+      todos[j].name = params.name
+      todos[j].description = params.description
+      todos[j].completed = params.completed
+      return true
+    }
+  }
+  return false
 }
 
 // DESTROY (destroy & destroyAll)
 function destroy (id) {
+  for ( var m = 0; m < todos.length; m++) {
+    if (todos[m]._id === id ) {
+      todos.splice(m, 1)
+      return true
+    }
+  }
+  return false
+}
+
+function destroyAll () {
+  todos.splice(0, todos.length)
+  return true
 }
 
 module.exports = {
@@ -36,5 +75,6 @@ module.exports = {
   list,
   show,
   update,
-  destroy
+  destroy,
+  destroyAll
 }
